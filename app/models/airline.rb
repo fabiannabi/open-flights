8480b6375c927fad0d2a73c13ef6e2331a541e2f
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-02 17:43:04 -0500

    Adds method to count average and slugify url

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,17 @@
 class Airline < ApplicationRecord
+  has_many :reviews
+
+  #this will run the method before its inserted in de DB
+  before_create :slugify
+
+  # this method will take the name before passing it to the data base and make it a slug
+  def slugify
+    # this is the correct way to  sets the value to the attribute
+    # parameterize("Donald E. Knuth") # => "donald-e-knuth"
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
 end
