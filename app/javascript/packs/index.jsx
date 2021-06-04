480ed98f7f1a5df9f48f3fa10ddb47e84c372008
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-04 12:15:46 -0500

    First confing in root app to use Router

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,11 +5,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App/>,
+    <BrowserRouter>
+      <Route path="/" component={App}/>
+    </BrowserRouter>
+      ,
     document.body.appendChild(document.createElement('div')),
   )
 })
