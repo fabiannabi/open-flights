cff563dd2ab6bb8a13347d836c33b041333f5b25
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-04 10:05:56 -0500

    Updates routes to have namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only:[:create, :destroy]
+    end
+  end
+
+  #this will ensure that if no path is find, it redirects to the page index root
+  get "*path", to: 'pages#index', via: :all
 end
