388d46f22a34366012a450e181720da269c94c9d
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-04 13:21:20 -0500

    Adds hooks to call to the db for airlines

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React ,  {useState, useEffect, Fragment} from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+
+	const [airlines, setAirlines] = useState([])
+
+	useEffect(() => {
+		//Get all of Airlines from the Api endpoints
+		//Update Airlines in our state
+		axios.get('/api/v1/airlines.json').then(response =>{
+			setAirlines(response.data.data)
+
+			}).catch(err => console.log(err))
+		},[airlines.length])
+
+	const list = airlines.map(airline =>{
+		return (
+			<li key={airline.attributes.name}>{airline.attributes.name}</li>
+		)
+	})
+
 	return (
-		<div>This is the Airlines show view for the app</div>
+		<Fragment>
+			<div>This is the Airlines show view for the app
+				<ul>
+					{list}
+				</ul>
+			</div>
+		</Fragment>
 	)
 }
 
