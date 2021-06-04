3a516316a5b87a8528c07870d3b065c66a130e12
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-04 17:27:31 -0500

    Creates Header componet for Airline

---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = (props) => {
+
+const { name, image_url , avg_score} = props.attributes
+
+const review = props.reviews.length
+
+console.log(review, "this is review")
+console.log(props)
+	return (
+		<div className="header-wrapper">
+			<h1 className="airline-name "><img src={image_url} alt={name}/>{name}</h1>
+			<div>
+				<div className="total-reviews">{ review } User reviews</div>
+				<div className="star-rating">stars</div>
+				<div className="total-out-of">{avg_score} out of 5</div>
+			</div>
+		</div>
+	)
+}
+
+export default Header
