1297fe4e2eb98d4beba897aafcc1f5f9d73002d7
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-04 15:46:38 -0500

    Creates Component for individual card of airline

--- app/javascript/components/Airlines/AirlineCard.js
+++ app/javascript/components/Airlines/AirlineCard.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const AirlineCard = (props) => {
-
-	console.log(props.attributes.name)
-
 	return (
 	<div className="card" key={props.attributes.slug}>
 		<div className="airline-logo">
 			<img src={props.attributes.image_url} alt={props.attributes.name}/>
-		</div>
+			</div>
 		<div className="airline-name">{props.attributes.name}
 		</div>
 		<div className="airline-score">{props.attributes.avg_score}</div>
 		<div className="airline-link">
-			<a href={`/airines/${props.attributes.slug}`}>VIEW MORE</a>
+			<Link  to={`/airines/${props.attributes.slug}`}>VIEW MORE</Link>
 		</div>
 	</div>
 )
-
 }
 
 export default AirlineCard
