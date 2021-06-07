34999e76d00da20df337b948e868345a76decfa5
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-07 12:06:05 -0500

    Updates handle funtions for form

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React ,  {useState, useEffect} from 'react'
+import React ,  {useState, useEffect, Fragment} from 'react'
 import Header from './Header'
+import ReviewForm from './ReviewForm'
 
 const Airline = (props) => {
 
@@ -15,35 +16,53 @@ const Airline = (props) => {
 	const [loaded, setLoaded] = useState(false)
 
 	useEffect(() => {
-		console.log(props)
 		const slug = props.match.params.slug
 
 		axios.get(`/api/v1/airlines/${slug}`).then((response) => {
-			console.log(response)
 			setAirline(response.data)
 			setLoaded(true)
 		}).catch((err) => consolelog(err) )
 	},[])
-	console.log(airline,"this is airline")
+
+
+	const handleChange = (e) => {
+		e.preventDefault()
+
+		console.log(e.target.name, e.target.value)
+
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(e.target.name, e.target.value)
+	}
+	// pass the handleSubmit and handleChange as props with the same name as params
+	// and pass the review state to fill it out in the form with the value
 	return (
+
 		<div className="wrapper">
+		{ loaded &&
+			<Fragment>
 			<div className="column-main">
 				<div className="main">
-					{ loaded &&
 						<Header
 							attributes ={airline.data.attributes}
 							reviews = {airline.included}
 						/>
-					}
 					<div className="reviews"></div>
 				</div>
 			</div>
 			<div className="column">
-				<div className="review-form">
-					review goes here
-				</div>
+				<ReviewForm
+					handleChange={handleChange}
+					handleSubmit={handleSubmit}
+					attributes={airline.data.attributes}
+					review={review}
+				/>
 			</div>
-		</div>
+	</Fragment>
+	}
+	</div>
 	)
 }
 
