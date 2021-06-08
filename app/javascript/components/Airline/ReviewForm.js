62d43cb1a5c04895b9f3d811c00225c4e1151c54
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-08 09:30:14 -0500

    Creates a rating sistem for the review form

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
 const ReviewForm = (props) => {
 
-	console.log(props)
+	const ratingOptions = [ 5,4,3,2,1].map((score, index)=>{
+		return(	<Fragment>
+				<input type="radio"
+					value={score}
+					name="rating"
+					defaultChecked={props.review.score == score}
+					onClick={props.setRating.bind(this, score)}
+					id={`rating-${score}`}/>
+				<div className="star" onClick={props.setRating.bind(this, score)}>{`${score}★`}</div>
+			</Fragment>
+		)
+	})
+
+	console.log(props.review.score)
+
 	return(
 		<div className="form-wrapper">
 			<form onSubmit={props.handleSubmit}>
-				<div>
+				<div className="headline">
 					Have an expirience with {props.attributes.name} ? Share your review!
 				</div>
 				<div className="field">
@@ -18,7 +32,10 @@ const ReviewForm = (props) => {
 				<div className="field">
 					<div className="rating-container">
 						<div className="rating-title">Rate this Airline</div>
-						[STAR RATIING GOES HERE]
+						<div className="raiting-box">
+						{ratingOptions}
+						</div>
+						<div className="rating-stars">Your rating {props.review.score}<span className="star-review">★</span></div>
 					</div>
 				</div>
 				<button className="submit" type="submit"> Sumbmit your review!</button>
